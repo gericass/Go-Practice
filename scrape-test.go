7255ce512729dfc275a607d4fccfd552a63092ff
高橋啓太 <Keita@takahashikeita-no-iMac.local>
2017-05-18 22:07:27 +0900

    scrape

--- scrape-test.go
+++ scrape-test.go
@@ -7,12 +7,21 @@ import (
 )
 
 func main() {
-	doc, err := goquery.NewDocument("https://github.com/PuerkitoBio/goquery")
+	doc, err := goquery.NewDocument("http://sense-sapporo.jp/release")
 	if err != nil {
 		fmt.Print("url scarapping failed")
 	}
-	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Attr("href")
-		fmt.Println(url)
+	doc.Find(".release_content_text").Each(func(i int, s *goquery.Selection) {
+
+		title := s.Find(".content_title_single").Text()
+
+		url, _ := s.Find("a").Attr("href")
+
+		fmt.Println(title, url)
 	})
+
+	/*
+		exit := doc.Find(".content_title_single").Each(func(i int,* sText()
+		println(exit)
+	*/
 }
---
+++ test/test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, ps := 3, 8
+	fmt.Println(ps)
+	fmt.Println(n)
+
+}
